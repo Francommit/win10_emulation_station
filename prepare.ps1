4a833830865960d00d1ab3da73ac8f37f05b0915
francommit <francommit@gmail.com>
2017-09-09 22:13:33 +1000

    Added downloader to prepare.ps1

--- prepare.ps1
+++ prepare.ps1
@@ -1,17 +1,38 @@
 Import-Module BitsTransfer
 
-# choco install directx -y
-# choco install emulationstation.install -y
+# 1. Chocolatey installs 
+choco install directx -y
+choco install emulationstation.install -y
 
-$UrlContents = Get-Content $PSScriptRoot\download_list.txt |  ForEach-Object {
+# 2. Acqurie files 
+$folder = "$PSScriptRoot\requirements\"
+New-Item -ItemType Directory -Force -Path $folder
 
-    $url = $_
-    $output = "$PSScriptRoot\" + ($_.split("{/}") | Select-Object -Last 1)
-    $start_time = Get-Date
+Get-Content download_list.json | ConvertFrom-Json | Select -expand downloads | ForEach-Object {
 
-    Start-BitsTransfer -Source $url -Destination $output
-    Write-Output "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+    $url = $_.url
+    $file = $_.file
+    $output = $folder + $file
+
+    if(![System.IO.File]::Exists($output)){
+
+        Write-Host $file "does not exist...Downloading."
+        Start-BitsTransfer -Source $url -Destination $output
+        Write-Host "Time taken: $((Get-Date).Subtract($start_time).Seconds) second(s)"
+
+    } else {
+
+        Write-Host $file "Already exists...Skipping download."
+
+    }
 
 }
+
+
+
+
+
+
+
 
 
