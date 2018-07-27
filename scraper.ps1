2382c079b6f88dbb1b992f404fb99d13c1544eea
Paulo Roberto Elias <ppuspfc@gmail.com>
2018-07-26 23:07:35 -0300

    Getting user input and final adjustments

--- scraper.ps1
+++ scraper.ps1
@@ -1,21 +1,35 @@
-# Configuring
-#Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
+# General info
+Write-host "This script is prepared for using ScreenScraper API, then you'll need to create a user there"
+Write-host "It relies on scraper.exe already downloaded by prepare script"
+Write-host "You gamelist file will be safe on a backup file on the same folder"
+Write-host "The script works for a single system each time, use name convention from Emulationstation"
+Write-host "Be patient and enjoy!"
+Write-host "--------------------------------------------------------------"
 
-$dir = $env:userprofile+"\.emulationstation\roms"
+# Configuring by user input
+$login = Read-Host -Prompt 'Input your ScreenScraper login'
+$password = Read-Host -Prompt 'Input your ScreenScraper password'
+$system = Read-Host -Prompt 'Which system do you want to scrap'
+Write-host "Are you using OneDriver folders for storing ROMs? (Default is No)"
+    $Readhost = Read-Host " ( y / n ) "
+    Switch ($ReadHost)
+     {
+       Y {$dir = $env:userprofile + "\OneDrive\.emulationstation\roms"}
+       N {$dir = $env:userprofile + "\.emulationstation\roms"}
+       Default {$dir = $env:userprofile + "\.emulationstation\roms"}
+     }
+
+# Setting variables
 $Command = "$dir\scraper.exe"
-
 $timestamp = Get-Date -Format FileDateTime
 
-#NES
-$console = "$dir\nes"
+# Running
+$console = "$dir\$system"
 Copy-Item $console\gamelist.xml "$console\gamelist$timestamp.xml"
-$Params_list = "-rom_dir $console -rom_path $console -download_videos -download_marquees -console_src=ss"
-$Params = $Params_list.Split("-")
+$Params_list = "&-rom_dir=$console &-rom_path=$console &-download_videos=true&-download_marquees=true&-console_src=ss&-ss_user=$login&-ss_password=$password&-image_dir=$console\images&-video_dir=$console\videos&-marquee_dir=$console\marquees"
+$Params = $Params_list.Split("&")
+Write-Host "$Command" $Params
 & "$Command" $Params
 
-#SNES
-$console = "$dir\snes"
-Copy-Item $console\gamelist.xml "$console\gamelist$timestamp.xml"
-$Params_list = "-rom_dir $console -rom_path $console -download_videos -download_marquees -console_src=ss"
-$Params = $Params_list.Split("-")
-& "$Command" $Params
+# Waiting for input to exit
+Read-Host -Prompt "Press Enter to exit!"
