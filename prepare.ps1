d02dde3e7eee681449919bb9e80b1b4e673f2254
Chris Franco <11533365+Francommit@users.noreply.github.com>
2020-02-18 00:20:01 +1100

    Updated TLS

--- prepare.ps1
+++ prepare.ps1
@@ -3,6 +3,7 @@ Set-ExecutionPolicy -ExecutionPolicy Bypass -Force
 
 Import-Module BitsTransfer
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls" #Convince Powershell to talk to sites with different versions of TLS
+[System.net.ServicePointManager]::SecurityProtocol = 3072 -bor 768 -bor 192 -bor 48
 
 # Installing Chocolatey 
 Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
