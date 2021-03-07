14c151d17481220f96660afe3abf110c11bed931
Chris Franco <11533365+Francommit@users.noreply.github.com>
2021-03-08 03:04:09 +1100

    Added GIT install before scoop

--- prepare.ps1
+++ prepare.ps1
@@ -72,6 +72,7 @@ Write-Host "INFO: Script directory is: $scriptDir"
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
+choco install git -y | Out-Null
 
 # Install and setup scoop
 if($env:path -match "scoop"){
