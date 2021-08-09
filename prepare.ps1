53fc6c0a32ed62187cb0aa9562b930ebed6549f0
Chris Franco <11533365+Francommit@users.noreply.github.com>
2021-08-09 19:49:20 +1000

    Debugging Powershell 3 SSL fix

--- prepare.ps1
+++ prepare.ps1
@@ -1,5 +1,3 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls -bor [Net.SecurityProtocolType]::Tls11 -bor [Net.SecurityProtocolType]::Tls12
-
 
 function DownloadFiles {
     param ([String]$jsonDownloadOption)
@@ -18,6 +16,20 @@ function DownloadFiles {
             if($PSVersionTable.PSEdition -eq "Core"){
                 Invoke-WebRequest $url -Out $output -SkipCertificateCheck
             } else {
+
+                add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+                [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
                 Invoke-WebRequest $url -Out $output 
                 
             }
