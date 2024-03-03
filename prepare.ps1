903b3d13d34f313a81d5f55edb8c682d80d79eff
Chris Franco <11533365+Francommit@users.noreply.github.com>
2024-03-04 01:29:37 +1100

    Debugging failure

--- prepare.ps1
+++ prepare.ps1
@@ -148,8 +148,8 @@ $retroArchPath = "$env:userprofile\.emulationstation\systems\retroarch\"
 $coresPath = "$retroArchPath\cores"
 $retroArchBinary = "$requirementsFolder\RetroArch.7z"
 if(Test-Path $retroArchBinary){
-    New-Item -ItemType Directory -Force -Path $retroArchPath | Out-Null
-    Expand-Archive -Path $retroArchBinary -Destination . | Out-Null
+    New-Item -ItemType Directory -Force -Path $retroArchPath 
+    Expand-Archive -Path $retroArchBinary -Destination . 
         # TO-DO - add an Out-Null when this has been tested
     Copy-Item -Path RetroArch-Win64\* -Destination $retroArchPath -recurse -Force
         # New path - $retroArchPath\RetroArch-Win64
